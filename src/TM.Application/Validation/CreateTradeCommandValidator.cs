20f6850120e6327339583eba650fbbd7ed4f8411
Maksym Pishchuk <maksym.pishchuk@unity-bars.com>
2024-05-22 01:06:13 +0300

    edit validation rules for create trade

--- src/TM.Application/Validation/CreateTradeCommandValidator.cs
+++ src/TM.Application/Validation/CreateTradeCommandValidator.cs
@@ -9,9 +9,9 @@ namespace TM.Application.Validation
         public CreateTradeCommandValidator()
         {
             RuleFor(x => x.TradeDTO.Profit)
-                .GreaterThan(0).When(x => string.Equals(x.TradeDTO.ResultType, ResultType.Take.ToString()))
-                .Equal(0).When(x => string.Equals(x.TradeDTO.ResultType,ResultType.BreakEven.ToString()) || x.TradeDTO.ResultType is null)
-                .Equal(q => -q.TradeDTO.RiskAmount).When(x => string.Equals(x.TradeDTO.ResultType, ResultType.Stop.ToString()));
+                .GreaterThan(0).When(x => string.Equals(x.TradeDTO.ResultType, ResultType.Take.ToString()), ApplyConditionTo.CurrentValidator)
+                .Equal(0).When(x => string.Equals(x.TradeDTO.ResultType,ResultType.BreakEven.ToString()) || x.TradeDTO.ResultType is null, ApplyConditionTo.CurrentValidator)
+                .Equal(q => -q.TradeDTO.RiskAmount).When(x => string.Equals(x.TradeDTO.ResultType, ResultType.Stop.ToString()), ApplyConditionTo.CurrentValidator);
 
             RuleFor(x => x.TradeDTO.DepositRisk).InclusiveBetween(0, 100).NotEmpty();
             RuleFor(x => x.TradeDTO.RiskAmount).LessThanOrEqualTo(q => q.TradeDTO.InitialDeposit);
@@ -23,20 +23,20 @@ namespace TM.Application.Validation
             
             RuleFor(x => x.TradeDTO.PriceTake)
                 .LessThan(x => x.TradeDTO.PriceEntry)
-                    .When(x => string.Equals(x.TradeDTO.PositionType, PositionType.Short.ToString()))
+                    .When(x => string.Equals(x.TradeDTO.PositionType, PositionType.Short.ToString()), ApplyConditionTo.CurrentValidator)
                 .GreaterThan(x => x.TradeDTO.PriceEntry)
-                    .When(x => string.Equals(x.TradeDTO.PositionType, PositionType.Long.ToString()))
+                    .When(x => string.Equals(x.TradeDTO.PositionType, PositionType.Long.ToString()), ApplyConditionTo.CurrentValidator)
                 .Equal(x => x.TradeDTO.PriceEntry)
-                    .When(x => string.Equals(x.TradeDTO.PositionType, PositionType.Market.ToString()))
+                    .When(x => string.Equals(x.TradeDTO.PositionType, PositionType.Market.ToString()), ApplyConditionTo.CurrentValidator)
                 .GreaterThan(0).NotEmpty();
 
             RuleFor(x => x.TradeDTO.PriceStop)
                 .GreaterThan(x => x.TradeDTO.PriceEntry)
-                    .When(x => string.Equals(x.TradeDTO.PositionType, PositionType.Short.ToString()))
+                    .When(x => string.Equals(x.TradeDTO.PositionType, PositionType.Short.ToString()), ApplyConditionTo.CurrentValidator)
                 .LessThan(x => x.TradeDTO.PriceEntry)
-                    .When(x => string.Equals(x.TradeDTO.PositionType, PositionType.Long.ToString()))
+                    .When(x => string.Equals(x.TradeDTO.PositionType, PositionType.Long.ToString()), ApplyConditionTo.CurrentValidator)
                 .Equal(x => x.TradeDTO.PriceEntry)
-                    .When(x => string.Equals(x.TradeDTO.PositionType, PositionType.Market.ToString()))
+                    .When(x => string.Equals(x.TradeDTO.PositionType, PositionType.Market.ToString()), ApplyConditionTo.CurrentValidator)
                 .GreaterThan(0).NotEmpty();
 
 
@@ -49,7 +49,7 @@ namespace TM.Application.Validation
             RuleFor(x => x.TradeDTO.SetupID).NotNull();
             RuleFor(x => x.TradeDTO.PairID).NotNull();
 
-            RuleFor(x => x.TradeDTO.Profit).NotNull().When(x => string.Equals(x.TradeDTO.ResultType, ResultType.EarlyExit.ToString()));
+            RuleFor(x => x.TradeDTO.Profit).NotNull().When(x => string.Equals(x.TradeDTO.ResultType, ResultType.EarlyExit.ToString()), ApplyConditionTo.CurrentValidator);
         }
     }
 }
